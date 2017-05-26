06858cf61b29df6ee3f23234eedd18cc66798777
Austin Tindle <tindleaj@gmail.com>
2017-05-25 20:46:56 -0400

    I give up on React for this project

--- src/components/news.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const News = () => {
-	return(
-		<div>News</div>
-	);
-}
-
-export default News;
--- src/components/research.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Research = () => {
-	return(
-		<div>Research</div>
-	);
-}
-
-export default Research;
