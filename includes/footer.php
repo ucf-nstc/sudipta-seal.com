7e860a0d29c10dbb2d21b1206f7d8c7c1c6e3085
Austin Tindle <tindleaj@gmail.com>
2017-05-25 22:29:13 -0400

    added footer info

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,22 @@
+<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Dr. Sudipta Seal</strong>, University of Central Florida &nbsp;|&nbsp; 
+        Director, <a href="">Advanced Materials Processing and Analysis Center</a> &nbsp;|&nbsp; 
+        Director, <a href="">NanoScience Technology Center</a>
+      </p>
+      <p>
+      	Professor, <a href="">Dept. of Mechanical, Materials, and Aerospace Engineering</a>
+      </p>
+      <p>
+      	Email: <a href="mailto:sudipta.seal@ucf.edu">Sudipta.Seal@ucf.edu</a> &nbsp;|&nbsp; 
+      	Phone: <a href="tel:+1407.823.5277">407.823.5277</a> or <a href="tel:+1407.882.1458">407.882.1458</a> &nbsp;|&nbsp;
+      	Fax: <a href="tel:+1407.882.1156">407.882.1156</a> or <a href="tel:+1407.823.0208">407.823.0208</a>
+      </p>
+    </div>
+  </div>
+</footer>
+
 </body>
 </html>
