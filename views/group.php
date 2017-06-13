6661414991b13b899010680706917eb36fd9dc33
Austin Tindle <tindleaj@gmail.com>
2017-06-13 10:14:37 -0400

    research group page funcionality added, group members displaying using the GET group route from the ucf-nstc/api

--- views/group.php
+++ views/group.php
@@ -1,7 +1,19 @@
 <?php
 
-/*
- */
+	/*
+	 * This page generates group members from the nanoscience database. If a group member in the group_members 
+	 * table has the research group name in the 'groups' column, then the group member will be 
+	 * displayed. The group is set using the $group variable below.
+	 * 
+	 * *** PLEASE DON'T ADD GROUP MEMBERS TO THIS FILE ***
+	 * Just put them in the database and make sure the correct 'groups' value is there.
+	 */
+
+	// TODO - refactor this out as a function to be used on other group sites
+
+
+$group = 'seal';
+$json = json_decode(file_get_contents('http://nanoscience.ucf.edu/api/get-group.php'));
 
 ?>
 
@@ -10,11 +22,34 @@
 	<hr>
 
 	<div>
-		
 	<?php
+		$title_index= array();
+		foreach($json as $member) {
+			$name = $member->firstName . " " . $member->lastName;
+			$image = $member->image;
+			if ($image === null) {
+				$image = 'blank.jpg';
+			}
 
+			if (strpos($member->groups, $group) !== false) {
+				// display the section header (member title) if the member is part of a new grouping
+				if (array_search($member->title, $title_index) === false) {
+					?>
+					</div>
+					<br><br>
+					<h2 class="title is-4"><?=$member->title?></h1>
+					<hr>
+					<div>
+					<?php
+					array_push($title_index, $member->title);
+				}
 
+				include('./views/partials/group-member.php');
+			}
+		}
 
 	?>
-
 	</div>
+	<br>
+	<br>
+</section>
---
+++ views/partials/group-member.php
@@ -0,0 +1,18 @@
+<article class="media">
+	<div class="media-left">
+		<figure class="image">
+			<img src="./public/images/group/<?=$image?>" alt="<?=$name?>" class="card">
+		</figure>
+	</div>
+	<div class="media-content">
+		<div class="content">
+			<h3 class="title is-5"><strong><?=$name?></strong></h3>
+			<p class="subtitle is-6">
+				<?=$member->affiliation?><br>
+				<?=$member->education?><br>
+				<a href="<?=$member->email?>"><?=$member->email?></a><br>
+				Research: <?=$member->research?><br>
+			</p>
+		</div>
+	</div>
+</article>
