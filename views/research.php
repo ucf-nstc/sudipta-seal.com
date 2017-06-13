3a4cdc5a911d029e18071f1fd8313ec5fc269972
Austin Tindle <tindleaj@gmail.com>
2017-06-13 15:19:43 -0400

    added research overview content

--- views/research.php
+++ views/research.php
@@ -1,4 +1,22 @@
 <section class="section">
 	<h1 class="title is-3">Research Overview</h1>
 	<hr>
+
+	<?php
+	// Pull research overview articles from the ./content/research directory. To add a new research article, add
+	// the markup to that directory
+	$dir = './content/research/';
+	$article_array = array();
+	$article_array = scandir($dir);
+	
+	foreach ($article_array as $article) {
+		if (strpos($article, 'html') !== false) {
+			include($dir . $article);
+		}
+	}
+
+	?>
 </section>
+
+<!-- Script for toggling article visibility -->
+<script type="text/javascript" src="../public/js/script.js"></script>
