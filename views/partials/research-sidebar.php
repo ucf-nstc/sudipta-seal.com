c995a856252c8f5a1a78e00aec3ee6e9c37d2943
Austin Tindle <tindleaj@gmail.com>
2017-06-13 18:19:33 -0400

    added initial research sidebar menu structure

---
+++ views/partials/research-sidebar.php
@@ -0,0 +1,9 @@
+<aside class="menu">
+	<p class="menu-label">Research</p>
+	<ul class="menu-list">
+	<li><a href="">Overview</a></li>
+	<li><a href="">Patents</a></li>
+	<li><a href="">Technology</a></li>
+	<li><a href="">Funding Sources</a></li>
+	</ul>
+</aside>
--- views/research.php
+++ views/research.php
@@ -1,22 +1,34 @@
 <section class="section">
-	<h1 class="title is-3">Research Overview</h1>
-	<hr>
+	<div class="columns">
 
-	<?php
-	// Pull research overview articles from the ./content/research directory. To add a new research article, add
-	// the markup to that directory
-	$dir = './content/research/';
-	$article_array = array();
-	$article_array = scandir($dir);
-	
-	foreach ($article_array as $article) {
-		if (strpos($article, '.html') !== false || strpos($article, '.php') !== false) {
-			include($dir . $article);
-			include('./views/partials/research-article.php');
-		}
-	}
+		<div class="column is-one-quarter">
+			<?php
+				// Sidebar
+				require('./views/partials/research-sidebar.php'); ?>
+		</div>
 
-	?>
+		<div class="column">
+			<h1 class="title is-3 is-spaced">Research</h1>
+			<h2 class="subtitle is-4">Overview</h2>
+			<hr>
+
+			<?php
+			// Pull research overview articles from the ./content/research directory. To add a new research article, add
+			// the markup to that directory
+			$dir = './content/research/';
+			$article_array = array();
+			$article_array = scandir($dir);
+			
+			foreach ($article_array as $article) {
+				if (strpos($article, '.html') !== false || strpos($article, '.php') !== false) {
+					include($dir . $article);
+					include('./views/partials/research-article.php');
+				}
+			}
+
+			?>
+		</div>
+	</div>
 </section>
 
 <!-- Script for toggling article visibility -->
