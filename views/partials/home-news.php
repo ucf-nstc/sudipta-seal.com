cf06a5e0ca2422792d1a2bc1b98284eefa648077
Austin Tindle <tindleaj@gmail.com>
2017-06-14 13:44:16 -0400

    fixed homepage news bug

--- views/partials/home-news.php
+++ views/partials/home-news.php
@@ -7,16 +7,26 @@ date_default_timezone_set('America/New_York');
 <p class="title is-3 is-highlighted-text"><strong>News</strong></p>
 <hr>
 <p>
+
 <?php
 	// Three news items by default
-	$index = 0;
+	$index = 1;
 	foreach ($json as $post) {
-		while ($index < 3) {
+
+		// small bug since php sees 0 == false and stringpos returns position of 
+		// string which sometimes is 0. Fixed with exact evaluation !==
+		if (strpos($post->groups, $group) !== false) {
 			include('./views/partials/news-post.php');
 			$index++;
 		}
+
+		// Not sure why the while loop didn't work on it's own
+		if ($index > 3) {
+			break;
+		}
 	}
 ?>
+
 </p>
 <br>
 <a href="./?page=news" class="button is-info">More news</a>
