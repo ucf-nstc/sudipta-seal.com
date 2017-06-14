c900a4f3c9005e45d83d0326f6a3f24c16d4fee4
Austin Tindle <tindleaj@gmail.com>
2017-06-14 13:33:20 -0400

    hide featured news on mobile

--- views/partials/news-feature.php
+++ views/partials/news-feature.php
@@ -1,4 +1,4 @@
-<div class="box notification">
+<div class="box notification is-hidden-mobile">
 	<h2 class="title is-4">Featured News</h2>
 	<div class="has-text-centered">
 		<iframe width="650" height="375" src="https://www.youtube.com/embed/7SuiNF-8GKE" frameborder="0" allowfullscreen></iframe>
