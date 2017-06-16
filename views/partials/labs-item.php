824e1d1d2e35719e68cada3b0b42f1191b130524
Austin Tindle <tindleaj@gmail.com>
2017-06-16 12:06:41 -0400

    added outline of labs page

---
+++ views/partials/labs-item.php
@@ -0,0 +1,16 @@
+<div class="box">
+	<article class="media">
+		<div class="media-content">
+			<div class="content">
+
+				<p>
+					<strong><?=$lab->name?></strong>
+					<br>
+					<?=$lab->description?>
+				</p>
+				<a class="button is-info" href="<?=$lab->link?>">Learn More</a>
+
+			</div>
+		</div>
+	</article>
+</div>
