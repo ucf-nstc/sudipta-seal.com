06c74ac272364ec4e04551fb9c313dd23e630911
Austin Tindle <tindleaj@gmail.com>
2017-06-20 09:20:38 -0400

    modified labs and plasma-lab

--- views/labs.php
+++ views/labs.php
@@ -1,3 +1,7 @@
+<?php 
+	$subpage = $_GET['page'];
+?>
+
 <section class="section">
 	<h1 class="title is-3 is-spaced">Labs</h1>
 	<h2 class="subtitle is-4"></h2>
@@ -5,13 +9,15 @@
 
 	<?php
 
-	$json = file_get_contents('./content/labs/labs.json');
-	$item_array = json_decode($json);
+		$json = file_get_contents('./content/labs/labs.json');
+		$item_array = json_decode($json);
 
-	// Returns each json object as a php object
-	foreach ($item_array as $lab) {
-		include('./views/partials/labs-item.php');
-	}
+		// Returns each json object as a php object
+		foreach ($item_array as $lab) {
+			include('./views/partials/labs-item.php');
+		}
 
 	?>
+
+
 </section>
