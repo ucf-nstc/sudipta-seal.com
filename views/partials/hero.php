8178f5b76bae4c84850db234f6a58c429b6a2be0
Austin Tindle <tindleaj@gmail.com>
2017-06-20 13:52:32 -0400

    added slider with old images

--- views/partials/hero.php
+++ views/partials/hero.php
@@ -1,11 +1,31 @@
-<?php 
+ <?php 
 	// Navigation
-	require_once('./views/partials/nav.php') ?>
+	require_once('./views/partials/nav.php');
+
+	$dir = './public/images/home/slider/';
+	$images = scandir($dir);
+?>
+
+<link href="./public/css/js-image-slider.css" rel="stylesheet" type="text/css" />
+<script src="./public/js/js-image-slider.js" type="text/javascript"></script>
+<link href="./public/css/generic.css" rel="stylesheet" type="text/css" />
 
 <!-- Slider -->
 <!-- Hero Body -->
 <div class="hero-body is-paddingless">
-	<figure class="image">
-	  <img src="./public/images/home/slider/1.jpg">
-	</figure>
-</div>
+	
+	<div id="sliderFrame">
+    <div id="slider">
+
+      <?php
+      	foreach ($images as $image) {
+					if (strpos($image, '.png') || strpos($image, '.jpg')) { ?>
+
+						<img src="<?=$dir . $image?>" alt="" class="slider-image"/>
+
+					<?php }
+				} ?>
+
+      </div>
+  </div>
+</div>	
