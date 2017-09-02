0f4e5cff7d60e8196aaefb6c70daad5b0a00ab84
Austin Tindle <tindleaj@gmail.com>
2017-09-01 21:35:15 -0400

    FIx current group member render bug, add a member

--- views/group/current.php
+++ views/group/current.php
@@ -4,29 +4,38 @@
 $group = 'seal';
 $json = json_decode(file_get_contents('http://nanoscience.ucf.edu/api/get-group.php'));
 
-$title_index= array();
+$title_index = array();
+
 foreach($json as $member) {
-	$name = $member->firstName . " " . $member->lastName;
-	$image = $member->image;
-	if ($image === null) {
-		$image = 'blank.jpg';
-	}
-
-	if (strpos($member->groups, $group) !== false) {
+	if (!array_search($member->title, $title_index)) {
 		if (!$member->alumni) {
-			// display the section header (member title) if the member is part of a new grouping
-			if (array_search($member->title, $title_index) === false) {
-				?>
-				</div>
-				<br><br>
-				<h2 class="title is-4"><?=$member->title?></h2>
-				<hr>
-				<div>
-				<?php
-				array_push($title_index, $member->title);
-			}
+			array_push($title_index, $member->title);
+		}
+	}
+}
 
-			include('./views/partials/group-member.php');
+foreach($title_index as $section) {
+	?>
+	</div>
+	<br><br>
+	<h2 class="title is-4"><?=$section?></h2>
+	<hr>
+	<div>
+	<?php
+
+	foreach($json as $member) {
+		$name = $member->firstName . " " . $member->lastName;
+		$image = $member->image;
+		if ($image === null) {
+			$image = 'blank.jpg';
+		}
+
+		if ($section === $member->title) {
+			if (strpos($member->groups, $group) !== false) {
+				if (!$member->alumni) {
+					include('./views/partials/group-member.php');
+				}
+			}
 		}
 	}
 }
